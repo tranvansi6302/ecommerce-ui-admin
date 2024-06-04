861838625798f7d316e13804e99125f2f2e1dd9c
Tran Van Si <tranvansi6302@gmail.com>
2024-06-04 14:30:40 +0700

    feature-auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import useRoutesElement from './routers/router'
+import { useEffect } from 'react'
+import PATH from './constants/path'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const navigate = useNavigate()
+    const location = useLocation()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        if (location.pathname === PATH.ROOT) {
+            navigate(PATH.DASHBOARD)
+        }
+    }, [location.pathname, navigate])
+
+    const elementRoutes = useRoutesElement()
+    return (
+        <main>
+            {elementRoutes}
+            <ToastContainer autoClose={600} />
+        </main>
+    )
 }
 
 export default App
