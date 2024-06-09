bc468c917d3da6dd46b1d5e2c63eae1def8c4eeb
Tran Van Si <tranvansi6302@gmail.com>
2024-06-09 08:27:00 +0700

    feature-create-category

--- src/apis/categories.api.ts
+++ src/apis/categories.api.ts
@@ -1,10 +1,13 @@
-import { CategoryResponse } from '~/@types/category'
+import { CategoryResponse, CreateCategoryResponse } from '~/@types/category'
 import API from '~/constants/api'
 import http from '~/utils/http'
 
 const categoriesApi = {
     getAllCategories: () => {
         return http.get<CategoryResponse>(API.CATEGORY)
+    },
+    createCategory: (data: { name: string }) => {
+        return http.post<CreateCategoryResponse>(API.CATEGORY, data)
     }
 }
 
